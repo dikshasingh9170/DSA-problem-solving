e4bb0277280c87ba7b656fef48109e93501b2f78
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2022-11-14 21:49:16 +0530

    Time: 38 ms (12.34%), Space: 54.5 MB (9.49%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> hs=new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i])){
+                hs.remove(nums[i]);
+            }
+            else{
+                hs.add(nums[i]);
+            }
+        }
+        int x=hs.iterator().next();
+        return x;
+    }
+}
