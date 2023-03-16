4baf9b6b478bbb4910d5dd9f600ee3b291c32d88
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-03-16 21:47:41 +0530

    Time: 2 ms (97.15%), Space: 42.4 MB (32.34%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,27 +15,23 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-                map.put(inorder[i], i);
-           
-        }
-        
-        return helper(map, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
-        
-    }
-    
-    private TreeNode helper(Map<Integer, Integer> map, int[] postorder, int inLeft, int inRight, int poLeft, int poRight) {
-        if (inLeft > inRight) {
+        if(inorder==null || postorder==null || inorder.length!=postorder.length){
             return null;
         }
-        
-        TreeNode root = new TreeNode(postorder[poRight]);
-        int inMid = map.get(root.val);
-        
-        root.left = helper(map, postorder, inLeft, inMid - 1, poLeft, poLeft + inMid - inLeft - 1);
-        root.right = helper(map, postorder, inMid + 1, inRight, poRight - inRight + inMid, poRight - 1);
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        for(int i=0;i<inorder.length;i++){
+            hm.put(inorder[i],i);
+        }
+        return helper(hm,postorder,0,inorder.length-1,0,postorder.length-1);
+    }
+    public TreeNode helper(HashMap<Integer,Integer> hm,int[] postorder,int istart,int iend,int pstart,int pend){
+        if(pstart>pend || istart>iend){
+            return null;
+        }
+        TreeNode root=new TreeNode(postorder[pend]);
+        int mid=hm.get(postorder[pend]);
+        root.left=helper(hm,postorder,istart,mid-1,pstart,pstart+mid-1-istart);
+        root.right=helper(hm,postorder,mid+1,iend,pstart+mid-istart,pend-1);
         return root;
     }
-	
 }
