a42e3ee6845d5e9296b4a3ff47e9405d6e0058a0
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-05-07 16:41:14 +0530

    Time: 26 ms (35.81%), Space: 42.9 MB (28.39%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,23 +1,23 @@
 class Solution {
-    public void dfs(Node node , Node copy , Node[] visited){
-        visited[copy.val] = copy;
-        for(Node n : node.neighbors){
-            if(visited[n.val] == null){
-                Node newNode = new Node(n.val);
-                copy.neighbors.add(newNode);
-                dfs(n , newNode , visited);
-            }else{
-                copy.neighbors.add(visited[n.val]);
-            }
-        }
-        
-    }
     public Node cloneGraph(Node node) {
-        if(node == null) return null; 
-        Node copy = new Node(node.val); 
-        Node[] visited = new Node[101]; 
-        Arrays.fill(visited , null); 
-        dfs(node , copy , visited); 
-        return copy; 
+         if(node == null) return null;
+          Map<Node, Node> map = new HashMap<>();
+          Queue<Node> queue = new ArrayDeque<>();
+ 
+    queue.add(node);
+    map.put(node, new Node(node.val, new ArrayList<>()));
+    while (!queue.isEmpty()) {
+        Node h = queue.poll();
+ 
+        for (Node neighbor : h.neighbors) {
+            if (!map.containsKey(neighbor)) {
+                map.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                queue.add(neighbor);
+            }
+            map.get(h).neighbors.add(map.get(neighbor));
+        }
+    }
+ 
+    return map.get(node);
     }
 }
