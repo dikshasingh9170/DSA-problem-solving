a5589fc2cfdaeb7a80a2bc3e0a034eef5db3d83e
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-05-21 11:31:13 +0530

    Time: 22 ms (38.05%), Space: 60.9 MB (10.73%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,27 @@
+//class Solution {
+    //public int findDuplicate(int[] nums) {
+       // HashSet<Integer> hs=new HashSet<Integer>();
+       // for(int i:nums){
+          //  if(hs.contains(i)){
+           //     return i;
+            //}
+               // hs.add(i);
+        //}
+        //return -1;
+    //}
+//}
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> hs=new HashSet<Integer>();
-        for(int i:nums){
-            if(hs.contains(i)){
-                return i;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int ans=0;
+        for(int item:nums){
+            if(map.containsKey(item)){
+                ans=item;
+                break;
             }
-                hs.add(i);
+            else
+                map.put(item,1);
         }
-        return -1;
+        return ans;
     }
 }
