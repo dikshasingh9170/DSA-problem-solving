c5c3adb7cf66035c3529753cb7025991cb93f6cc
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-04 00:55:32 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) {
+        if(head==null){
+            return null;
+        }
+        Node rev=reverse1(head);
+        Node temp=rev;
+        while(rev!=null){
+            if(rev.data<9){
+                rev.data+=1;
+                return reverse1(temp);
+            }
+            rev.data=0;
+            rev=rev.next;
+        }
+        Node nNode=new Node(1);
+        nNode.next=temp;
+        return nNode;
+    }
+    public static Node reverse1(Node head){
+        Node prev=null;
+        while(head!=null){
+            Node temp=head.next;
+            head.next=prev;
+            prev=head;
+            head=temp;
+        }
+        return prev;
+    }
+}
