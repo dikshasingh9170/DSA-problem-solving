e04dc9d1a1441fadf728f71dd9862cc4bc0c1b7e
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-12 15:04:19 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.java
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.java
@@ -27,17 +27,19 @@ import java.util.*;
         }
     }
 }
+// } Driver Code Ends
+
+
+// User function Template for Java
+
 class Solution {
     int getMinDiff(int[] arr, int n, int k) {
-        Arrays.sort(arr);
-        int diff=arr[n-1]-arr[0];
-        int min=0;int max=0;
+        Arrays.sort(arr);int max=0;
+        int min=0;int diff=(arr[n-1]-arr[0]);
         for(int i=1;i<n;i++){
             if(arr[i]-k<0)continue;
-
             max=Math.max(arr[i-1]+k,arr[n-1]-k);
             min=Math.min(arr[0]+k,arr[i]-k);
-
             diff=Math.min(diff,max-min);
         }
         return diff;
