82da118f8260b2310457adbf244c869f86e4cbf7
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-14 14:35:36 +0530

    Time: 1 ms (99.68%), Space: 48.4 MB (31.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,26 @@
+//4 approaches
+//1:-time O(n2) space:-O(1)
+//iterate the array for each element and count the number of occurences
+//2:-time O(n) space:-O(1)
+//take hashmap store each element into it and then its occurences as value
+//3:-Moore voting algorithm
+//time:-O(n) space:-O(1)
 class Solution {
     public int majorityElement(int[] nums) {
-        int count1=0;
+        int count=0;
         int candidate=0;
         for(int i=0;i<nums.length;i++){
-            if(count1==0){
+            if(count==0){
                 candidate=nums[i];
             }
-            
-            if(nums[i]==candidate){
-                count1++;
+            if(candidate==nums[i]){
+                count++;
             }
             else{
-                count1--;
+                count--;
             }
         }
-        return candidate;
+            return candidate;
+        
     }
 }
