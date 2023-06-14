a07f1475b56ed7a9ef555e1782e27691c04fd7cb
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-14 15:38:57 +0530

    Time: 2 ms (22.62%), Space: 39.7 MB (86.34%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,19 @@
 class Solution {
+    static double eps = 1e-6;
     public int mySqrt(int x) {
-        return (int)Math.floor(Math.sqrt(x));
+        double l = 1, h = x, m=0;
+        while (h - l > eps)
+        {
+            m = (l + h) / 2;
+            if (m * m < x)
+            {
+                l = m;
+            }
+            else
+            {
+                h = m;
+            }
+        }
+        return (int)h;
     }
 }
