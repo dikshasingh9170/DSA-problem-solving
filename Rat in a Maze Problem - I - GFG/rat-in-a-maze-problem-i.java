61d8f87802a484fa96f348d21596caed08a5db75
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-15 15:24:01 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -29,6 +29,7 @@ class Rat {
 
 // } Driver Code Ends
 
+
 class Solution {
     static  ArrayList<String> res;
     
@@ -43,31 +44,19 @@ class Solution {
         return res;
     }
     public static void findpaths(int i,int j,int[][] m,int n,boolean[][] vis,String s){
+        if(i<0 || j<0 || i>n-1 || j>n-1 || m[i][j]==0 || vis[i][j]==true){
+            return;
+        }
         if(i==n-1 && j==n-1){
             res.add(s);
             return;
         }
-        else{
-            vis[i][j]=true;
-        }
-        if(isvalid(i+1,j,n,m) && vis[i+1][j]==false){
-            findpaths(i+1,j,m,n,vis,s+'D');
-        }
-        if(isvalid(i-1,j,n,m) && vis[i-1][j]==false){
-            findpaths(i-1,j,m,n,vis,s+'U');
-        }
-        if(isvalid(i,j-1,n,m) && vis[i][j-1]==false){
-            findpaths(i,j-1,m,n,vis,s+'L');
-        }
-        if(isvalid(i,j+1,n,m) && vis[i][j+1]==false){
-            findpaths(i,j+1,m,n,vis,s+'R');
-        }
+        vis[i][j]=true;
+        findpaths(i+1,j,m,n,vis,s+'D');
+        findpaths(i-1,j,m,n,vis,s+'U');
+        findpaths(i,j-1,m,n,vis,s+'L');
+        findpaths(i,j+1,m,n,vis,s+'R');
         vis[i][j]=false;
-    }
-     public static boolean isvalid(int i,int j,int n,int[][] m){
-        if(i>=0 && i<=n-1 && j>=0 && j<=n-1 && m[i][j]==1){
-            return true;
-        }
-        return false;
+        return;
     }
 }
