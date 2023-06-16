975dcc473d6bfb670501dd892ada90d7b08f6350
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-16 14:49:30 +0530

    Time: 42 ms (73.62%), Space: 41.1 MB (93.84%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,53 @@
+/*class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        return solve(0,nums,sum/2);
+    }
+    public boolean solve(int i,int[] nums,int sum){
+        if(sum==0){
+            return true;
+        }
+        if(i>=nums.length && sum!=0){
+            return false;
+        }
+        if(nums[i]>sum){
+            return solve(i+1,nums,sum);
+        }
+        return solve(i+1,nums,sum-nums[i]) || solve(i+1,nums,sum);
+    }
+}*/
+class Solution {
+    
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        
+        for(int i : nums) sum+=i;
+        
+        if(sum%2!=0) return false;
+        
+        sum /= 2;
+        
+        boolean[] dp = new boolean[sum+1];
+       
+        dp[0] = true;
+
+        for (int j : nums) {
+            for (int i = sum; i > 0; i--) {
+                if (i >= j) {
+                    dp[i] = dp[i] || dp[i-j];
+                }
+            }
+        }
+
+        return dp[sum];
+        
+        
+    }
+}
