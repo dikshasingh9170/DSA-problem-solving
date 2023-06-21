a8943a6632dcbc0f2c5a2727db80716aad7c9f39
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-21 12:52:15 +0530

    Time: 272 ms (30.18%), Space: 55 MB (83.29%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,5 +1,8 @@
-/*class Solution {
+class Solution {
     public int largestRectangleArea(int[] heights) {
+        if(heights.length==1){
+            return heights[0];
+        }
         Stack<Integer> st=new Stack<Integer>();
         int n=heights.length;
         int[] leftsmaller=new int[n];
@@ -16,6 +19,7 @@
                 }
                 st.push(i);
         }
+        st=new Stack<>();
         for(int i=n-1;i>=0;i--){
                 while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                     st.pop();
@@ -24,7 +28,7 @@
                     rightsmaller[i]=st.peek();
                 }
                 else{
-                    rightsmaller[i]=-1;
+                    rightsmaller[i]=n;
                 }
                 st.push(i);
         }
@@ -35,56 +39,4 @@
         }
         return max;
     }
-}*/
-class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int smallleft[] = new int[n];
-        int smallright[] = new int[n];
-        Stack<Integer> s = new Stack<>();
-
-        //small left index
-        for(int i=0;i<heights.length;i++){
-            while(!s.isEmpty() && heights[s.peek()] >= heights[i] ){
-                s.pop();
-            }
-            if(s.isEmpty()){
-                smallleft[i] = -1;
-            }
-            else{
-                smallleft[i] = s.peek();
-            }
-
-            s.push(i);
-        }
-
-        //small right index
-        s = new Stack<>();
-        for(int i=heights.length-1; i>=0; i--){
-            while(!s.isEmpty() && heights[s.peek()] >= heights[i] ){
-                s.pop();
-            }
-            if(s.isEmpty()){
-                smallright[i] = heights.length;
-            }
-            else{
-                smallright[i] = s.peek();
-            }
-
-            s.push(i);
-        }
-        int largeRectangle = 0;
-        for(int i=0; i<heights.length; i++){
-            int height = heights[i];
-            int width = smallright[i] -smallleft[i]-1;
-            int currRectangle = height * width;
-            largeRectangle = Math.max(currRectangle, largeRectangle);
-        }
-
-        return largeRectangle;
-
-
-    }
 }
-
-
