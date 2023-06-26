5e0fbc4596b599df7067b2d08d6d372b436466cb
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-26 10:49:45 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (19.38%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -7,14 +7,14 @@ class Solution {
         return res;
     }
     public void powerset(int i,int[] nums, List<List<Integer>> res,ArrayList<Integer> arr){
-        /*res.add(new ArrayList<>(arr));
+        res.add(new ArrayList<>(arr));
         for(int index = i;index<nums.length;index++) {
             if(index!=i && nums[index] == nums[index-1]) continue; 
             arr.add(nums[index]); 
             powerset(index+1, nums, res, arr); 
             arr.remove(arr.size() - 1);
-        }*/
-        if(i==nums.length){
+        }
+        /*if(i==nums.length){
             
                 res.add(new ArrayList<>(arr));
             return;
@@ -26,6 +26,6 @@ class Solution {
         while(i+1<nums.length && nums[i]==nums[i+1]){
             i++;
         }
-        powerset(i+1,nums,res,arr);
+        powerset(i+1,nums,res,arr);*/
     }
 }
