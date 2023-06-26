9a9060ad125eaca53f0c95fc512afa60389c0ac4
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-26 11:03:10 +0530

    Time: 1 ms (98.89%), Space: 43.9 MB (43.04%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        //t.c for this code is n!*n
+        //tc :- n!*n
+        List<List<Integer>> res=new ArrayList<>();
+        solve(0,nums,res);
+        return res;
+    }
+    public void solve(int i,int[] nums,List<List<Integer>> res){
+        if(i==nums.length){
+            List<Integer> arr=new ArrayList<Integer>();
+            for(int j=0;j<nums.length;j++){
+                arr.add(nums[j]);
+            }
+            res.add(new ArrayList<>(arr));
+            return;
+        }
+        for(int j=i;j<nums.length;j++){
+            swap(i,j,nums);
+            solve(i+1,nums,res);
+            swap(j,i,nums);
+        }
+    }
+    public void swap(int x,int y,int[] nums){
+        int temp=nums[x];
+        nums[x]=nums[y];
+        nums[y]=temp;
+    }
+}
+//t.c for this code is n!*n
         //s.c is o(n)+O(n)
         /*List<List<Integer>> res=new ArrayList<>();
         List<Integer> ans=new ArrayList<Integer>();
@@ -22,28 +48,3 @@ class Solution {
                 freq[i]=false;
             }
         }*/
-        List<List<Integer>> res=new ArrayList<>();
-        solve(0,nums,res);
-        return res;
-    }
-    public void solve(int i,int[] nums,List<List<Integer>> res){
-        if(i==nums.length){
-            List<Integer> ans=new ArrayList<Integer>();
-            for(int j=0;j<nums.length;j++){
-                ans.add(nums[j]);
-            }
-            res.add(new ArrayList<>(ans));
-            return;
-        }
-        for(int j=i;j<nums.length;j++){
-            swap(j,i,nums);
-            solve(i+1,nums,res);
-            swap(j,i,nums);
-        }
-    }
-    public void swap(int x,int y,int[] nums){
-        int temp=nums[x];
-        nums[x]=nums[y];
-        nums[y]=temp;
-    }
-}
