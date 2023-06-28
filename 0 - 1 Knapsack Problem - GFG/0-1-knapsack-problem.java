ba574e49c8686687e0f58b71592ab2dc98e0a2b3
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-28 20:18:49 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -40,30 +40,27 @@ class gfg
     }
 }
 
-
-
-
-// } Driver Code Ends
-
-
 class Solution 
 { 
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-        int[] dp = new int[W + 1];
-        
-        for(int i=wt[0]; i<=W; i++){
-            dp[i] = val[0];
-        }    
-        for (int i = 1; i <n; i++) {
-            for (int w = W; w>=0; w--) {
-                if (wt[i] <= w){
-                    dp[w]= Math.max(dp[w],dp[w-wt[i]]+ val[i]);
-                }
+        int i, w;
+        int K[][] = new int[n + 1][W + 1];
+ 
+        // Build table K[][] in bottom up manner
+        for (i = 0; i <= n; i++) {
+            for (w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    K[i][w] = 0;
+                else if (wt[i - 1] <= w)
+                    K[i][w]= Math.max(val[i - 1]
+                                  + K[i - 1][w - wt[i - 1]],
+                              K[i - 1][w]);
+                else
+                    K[i][w] = K[i - 1][w];
             }
         }
-        return dp[W]; 
+ 
+        return K[n][W];
     }
 }
-
-
