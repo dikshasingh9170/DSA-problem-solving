d5a5b0a59640d1fe54843b675c3515b045fb5f3d
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-29 14:28:42 +0530

    Time: 69 ms (47.88%), Space: 46.7 MB (49.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,6 +1,6 @@
-/*class Solution {
+class Solution {
     public boolean canPartition(int[] nums) {
-        int sum=0;
+        /*int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
@@ -20,9 +20,37 @@
             return solve(i+1,nums,sum);
         }
         return solve(i+1,nums,sum-nums[i]) || solve(i+1,nums,sum);
+    }*/
+    int sum=0;
+    for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
     }
-}*/
-class Solution {
+    if(sum%2!=0){
+        return false;
+    }   
+        sum=sum/2;
+        int N=nums.length;
+    boolean[][] t=new boolean[N+1][sum+1];
+        for(int i=0;i<=N;i++){
+            t[i][0]=true;
+        }
+        for(int i=1;i<=sum;i++){
+            t[0][i]=false;
+        }
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=sum;j++){
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j-nums[i-1]] || t[i-1][j];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[N][sum];  
+    }
+}
+/*class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
         int n = nums.length;
@@ -40,4 +68,4 @@ class Solution {
         }
         return dp[sum];
     }
-}
+}*/
