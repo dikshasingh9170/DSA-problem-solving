e65d4b94fcf2564e52cf0e62f1d731d37b624b9b
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-06-29 15:01:09 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -43,7 +43,7 @@ class Solution{
             t[0][i]=false;
         }
         for(int i=1;i<=N;i++){
-            for(int j=1;j<=sum;j++){
+            for(int j=0;j<=sum;j++){
                 if(arr[i-1]<=j){
                     t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
                 }
