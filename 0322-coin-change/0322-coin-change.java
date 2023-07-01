659296d427677c2a3d6d414f5fb9ddf5fd1a7423
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-01 17:47:00 +0530

    Time: 26 ms (37.62%), Space: 44.2 MB (12.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,36 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount==0)return 0;
-        int[] dp=new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.length;j++){
-                if(i-coins[j]>=0 && dp[i-coins[j]]!=Integer.MAX_VALUE){
-                    dp[i]=Math.min(dp[i],1+dp[i-coins[j]]);
+        int W=amount;
+        int n=coins.length;
+        int[][] t=new int[n+1][W+1];
+        for(int i=0;i<=W;i++){
+            t[0][i]=Integer.MAX_VALUE-1;
+        }
+        for(int i=1;i<=n;i++){
+            t[i][0]=0;
+        }
+        int j=1;
+        for(int i=1;j<=W;j++){
+            if(j%coins[i-1]==0){
+                t[i][j]=j/coins[i-1];
+            }
+            else{
+                t[i][j]=Integer.MAX_VALUE-1;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for( j=1;j<=W;j++){
+                if(coins[i-1]<=j){
+                    t[i][j]=Math.min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                }
+                else if(coins[i-1]>j){
+                    t[i][j]=t[i-1][j];
                 }
             }
         }
-        if(dp[amount]==Integer.MAX_VALUE){
-            return -1;
+        if(t[n][W]==Integer.MAX_VALUE-1){
+            t[n][W]=-1;
         }
-        return dp[amount];
+        return t[n][W];
     }
 }
