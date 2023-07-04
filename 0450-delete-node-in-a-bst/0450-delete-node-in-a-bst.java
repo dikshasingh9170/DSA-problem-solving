8603c2641b4640df74874e8777261e714af47a8d
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-04 15:46:19 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (7.76%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,46 +14,34 @@
  * }
  */
 class Solution {
-
-    public TreeNode InorderSuccessors(TreeNode root){
+    public TreeNode min(TreeNode root){
         while(root.left != null){
             root = root.left;
         }
-
         return root;
     }
-
     public TreeNode deleteNode(TreeNode root, int key) {
-
         if(root == null){
             return null;
         }
-
         if(root.val < key){
             root.right = deleteNode(root.right, key);
         }
         else if(root.val > key){
             root.left = deleteNode(root.left, key);
         }else{
-
             if(root.left == null && root.right == null){
                 return null;
             }
-
             if(root.left == null){
                 return root.right;
             }else if(root.right == null){
                  return root.left;
             }
-
-            TreeNode IS = InorderSuccessors(root.right);
+            TreeNode IS =min(root.right);
             root.val = IS.val;
             root.right = deleteNode(root.right, IS.val);
-            
-            
             }
-
-  return root;   
-}
-
+    return root;   
+    }
 }
