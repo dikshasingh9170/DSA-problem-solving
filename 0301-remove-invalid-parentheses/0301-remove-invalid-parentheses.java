2942b84f653fc28c8c071669d190d287220a7f60
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-08 17:12:10 +0530

    Time: 138 ms (34.71%), Space: 48.7 MB (9.97%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -3,28 +3,24 @@ class Solution {
         List<String> res=new ArrayList<>();
         HashSet<String> hs=new HashSet<String>();
         int min=removebracket(s);
-        System.out.println(min);
         solve(min,s,res,hs);
         return res;
     }
-    public void solve(int min,String s,List<String> res,HashSet<String> hs){
-        if(hs.contains(s))return;
-        
-        hs.add(s);
-        
+    public void solve(int min,String s,List<String> res,HashSet<String> set){
+        if(set.contains(s))return;
+        set.add(s);
         if(min==0){
-            int remove=removebracket(s);   
-            if(remove==0) res.add(s);
+            int remove=removebracket(s);
+            if(remove==0){
+                res.add(s);
+            }
             return;
         }
-        
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!='(' && s.charAt(i)!=')')continue;
-            String L=s.substring(0,i);
-            String R=s.substring(i+1);
-            if(!hs.contains(L+R)){
-                System.out.println(L+R+" "+min);
-                solve(min-1,L+R,res,hs);
+            String l=s.substring(0,i);
+            String r=s.substring(i+1);
+            if(!set.contains(l+r)){
+                solve(min-1,l+r,res,set);
             }
         }
     }
