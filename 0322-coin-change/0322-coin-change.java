e0c4421c98621daf1c5024f8f11808a5cb2fefb2
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-14 11:31:44 +0530

    Time: 31 ms (32.25%), Space: 43.6 MB (38.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int coinChange(int[] coins, int amount) {
         int W=amount;
         int n=coins.length;
@@ -32,5 +32,36 @@ class Solution {
             t[n][W]=-1;
         }
         return t[n][W];
+    }
+}*/
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,-1);
+        int ans=solve(coins,amount,dp);
+        if(ans==Integer.MAX_VALUE){
+            return -1;
+        }
+        return ans;
+    }
+    public int solve(int[] coins,int amount,int[] dp){
+        if(amount==0){
+            return 0;
+        }
+        if(amount<0){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[amount]!=-1){
+            return dp[amount];
+        }
+        int mini=Integer.MAX_VALUE;
+        for(int i=0;i<coins.length;i++){
+            int ans=solve(coins,amount-coins[i],dp);
+            if(ans!=Integer.MAX_VALUE){
+                mini=Math.min(mini,1+ans);
+            }
+        }
+        dp[amount]=mini;
+        return mini;
     }
 }
