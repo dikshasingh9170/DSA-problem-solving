24f9f844388471393e7db317b6deedf2295f3168
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-14 10:27:37 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (44.59%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int fib(int n) {
         if(n==0){
             return 0;
@@ -15,5 +15,35 @@ class Solution {
             prev2=curr;
         }
         return curr;
+    }
+}*/
+/*class Solution {
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n-1,dp);
+    }
+    public int solve(int i,int[] dp){
+        if(i<=1){
+            return i;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        dp[i]=solve(i-1,dp)+solve(i-2,dp);
+        return dp[i];
+    }
+}*/
+class Solution {
+    public int fib(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
