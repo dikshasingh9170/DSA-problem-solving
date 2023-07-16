ec800f0a9b973d1b2937a3ac0318c38e5d0c8e19
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-16 15:39:52 +0530

    Time: 43 ms (58.45%), Space: 43 MB (33.98%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,10 +1,19 @@
 class Solution {
     public int numSquares(int n) {
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=n;j++){
+                int temp=j*j;
+                if(i-temp>=0){
+                    dp[i]=Math.min(dp[i],1+dp[i-temp]);
+                }
+            }
+        }
+        return dp[n];
     }
-    public int solve(int n,int[] dp){
+    /*public int solve(int n,int[] dp){
         if(n==0){
             return 0;
         }
@@ -17,5 +26,5 @@ class Solution {
         }
         dp[n]=ans;
         return dp[n];
-    }
+    }*/
 }
