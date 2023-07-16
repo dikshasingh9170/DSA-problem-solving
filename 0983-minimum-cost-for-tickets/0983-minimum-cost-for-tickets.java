7032cc5c861626fdb9c578cdb1913114dc31e477
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-16 20:03:18 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (86.64%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,17 +1,26 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        int[] dp = new int[366];
-        Set<Integer> travelDays = new HashSet<>();
-        for (int day : days) {
-            travelDays.add(day);
-        }
-        for (int i = 1; i <= 365; i++) {
-            if (!travelDays.contains(i)) {
-                dp[i] = dp[i-1];
-            } else {
-                dp[i] = Math.min(dp[i-1] + costs[0], Math.min(dp[Math.max(0, i-7)] + costs[1], dp[Math.max(0, i-30)] + costs[2]));
-            }
-        }
-        return dp[365];
+        int dp[]=new int[days.length];
+        for(int i=0;i<days.length;i++)
+            dp[i]=-1;
+        return solve(days,costs,0,dp);
+    }
+    public int solve(int[] days, int[] costs,int ind, int dp[])
+    {
+        if(ind>=days.length)
+            return 0;
+        if(dp[ind]!=-1)
+           return dp[ind];
+        int d1=costs[0]+solve(days,costs,ind+1,dp);
+        
+        int i=0;
+        for(i=ind;i<days.length&&days[i]<days[ind]+7;i++);
+        int d7=costs[1]+solve(days,costs,i,dp);    
+        
+        for( i=ind;i<days.length&&days[i]<days[ind]+30;i++);
+        int d30=costs[2]+solve(days,costs,i,dp);    
+
+        dp[ind]= Math.min(d1,Math.min(d7,d30));
+        return dp[ind];
     }
 }
