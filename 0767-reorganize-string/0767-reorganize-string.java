d30c4f14e2c931e18a9ac2568c24f2dd3c8486f8
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-07-18 11:25:08 +0530

    Time: 1 ms (84.84%), Space: 40.1 MB (99.68%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String reorganizeString(String S){
         Map<Character, Integer> map = new HashMap<>();
         for (char c : S.toCharArray()) {
@@ -29,4 +29,36 @@ class Solution {
         }
         return sb.toString();
     }
+}*/
+class Solution {
+    public String reorganizeString(String s) {
+        int m[]=new int[26];//Hashmap to store the frequency of each character
+        int n=s.length();
+        for(int i=0;i<n;i++)
+            m[s.charAt(i)-'a']++;
+        
+        StringBuilder ans=new StringBuilder();
+        int i=0;
+        char prev='*';//Keep track of previous character
+        while(i<n){
+            int maxi=0,ind=0;
+            //We traverse the entire hashmap to find the character with maximum count and not equal to previous character
+            for(int j=0;j<26;j++){                
+                if(m[j]>maxi&&prev!=(j+'a')&&m[j]>0){
+                    maxi=m[j];
+                    ind=j;
+                }
+            }
+            if(maxi==0) return "";//If maxi is 0 then no solution can be made as suitable element not found
+            prev=(char)(ind+'a');//Updating previous
+            ans.append(prev);
+            m[ind]--;  //Decreasing count from map since the character is done
+            i++;
+        }        
+        return ans.toString();
+    }
 }
+/*
+    "aaabcc"
+    acacab
+*/
