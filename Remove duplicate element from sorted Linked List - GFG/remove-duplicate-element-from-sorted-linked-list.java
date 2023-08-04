865cf16b5b38cbde22e83e838805a95d6d5a73aa
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-08-04 19:13:11 +0530

    Added solution - LeetHub

--- Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.java
+++ Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.java
@@ -73,17 +73,18 @@ class Node
 
 class GfG
 {
+    //Function to remove duplicates from sorted linked list.
     Node removeDuplicates(Node head)
     {
         Node curr=head;
-	    while(curr.next!=null){
-	        if(curr.data==curr.next.data){
-	            curr.next=curr.next.next;
-	        }
-	        else{
-	            curr=curr.next;
-	        }
-	    }
-	    return head;
+        while(curr.next!=null){
+            if(curr.data==curr.next.data){
+                curr.next=curr.next.next;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        return head;
     }
 }
