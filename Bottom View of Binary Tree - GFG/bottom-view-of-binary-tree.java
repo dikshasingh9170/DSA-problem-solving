deccad47aaeab03e54e905e79462dea7f32cf857
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-08-09 14:05:10 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
@@ -122,12 +122,12 @@ class Solution
     //Function to return a list containing the bottom view of the given tree.
     public ArrayList <Integer> bottomView(Node root)
     {
+        TreeMap<Integer,Integer> hm=new TreeMap<Integer,Integer>();
         Queue<Pair> q=new LinkedList<>();
-        TreeMap<Integer,Integer> map=new TreeMap<>();
         q.add(new Pair(0,root));
         while(!q.isEmpty()){
             Pair curr=q.poll();
-            map.put(curr.hd,curr.node.data);
+            hm.put(curr.hd,curr.node.data);
             if(curr.node.left!=null){
                 q.add(new Pair(curr.hd-1,curr.node.left));
             }
@@ -135,11 +135,11 @@ class Solution
                 q.add(new Pair(curr.hd+1,curr.node.right));
             }
         }
-        ArrayList<Integer> ans=new ArrayList<Integer>();
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            ans.add(entry.getValue());
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        for(int e:hm.values()){
+            arr.add(e);
         }
-        return ans;
+        return arr;
     }
 }
 class Pair{
