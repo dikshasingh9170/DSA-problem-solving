201797b7f67d6eb8b7ce278147d0fb109e6c364b
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-08-09 09:09:09 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (7.97%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -16,13 +16,26 @@
 class Solution {
     public TreeNode prev=null;
     public void flatten(TreeNode root) {
-        if(root==null){
+        /*if(root==null){
             return;
         }
         flatten(root.right);
         flatten(root.left);
         root.right=prev;
         root.left=null;
-        prev=root;
+        prev=root;*/
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                prev=curr.left;
+                while(prev.right!=null){
+                    prev=prev.right;
+                }
+                prev.right=curr.right;
+                curr.right=curr.left;
+                curr.left=null;
+            }
+            curr=curr.right;
+        }
     }
 }
