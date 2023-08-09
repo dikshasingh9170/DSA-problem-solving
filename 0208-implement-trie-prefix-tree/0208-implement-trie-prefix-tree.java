f60dbf87207ce447e823b7bc92a6f58502558aa8
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-08-09 10:29:08 +0530

    Time: 31 ms (99.48%), Space: 54.8 MB (51.38%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,31 +1,27 @@
-class TrieNode {
+class TrieNode{
     boolean isCompleteWord;
     TrieNode[] children;
-    
-    public TrieNode() {
-        isCompleteWord = false; 
-        children = new TrieNode[26]; 
+    public TrieNode(){
+        isCompleteWord=false;
+        children=new TrieNode[26];
     }
 }
-class Trie {
+class Trie{
     TrieNode root;
-    
-    public Trie() {
-        root = new TrieNode();
+    public Trie(){
+        root=new TrieNode();
     }
-    
-    public void insert(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
-                node.children[index] = new TrieNode();
+    public void insert(String word){
+        TrieNode node=root;
+        for(char c:word.toCharArray()){
+            int index=c-'a';
+            if(node.children[index]==null){
+                node.children[index]=new TrieNode();
             }
-            node = node.children[index];
+            node=node.children[index];
         }
-        node.isCompleteWord = true;
+        node.isCompleteWord=true;
     }
-    
     public boolean search(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
@@ -37,10 +33,9 @@ class Trie {
         }
         return node.isCompleteWord;
     }
-    
-    public boolean startsWith(String prefix) {
+    public boolean startsWith(String word) {
         TrieNode node = root;
-        for (char c : prefix.toCharArray()) {
+        for (char c : word.toCharArray()) {
             int index = c - 'a';
             if (node.children[index] == null) {
                 return false;
@@ -50,3 +45,22 @@ class Trie {
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
