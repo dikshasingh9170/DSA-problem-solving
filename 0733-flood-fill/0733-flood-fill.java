48bf516cc7d6354839a4ba5f37afbe7380ea37d8
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-08-23 00:09:00 +0530

    Time: 1 ms (49.60%), Space: 44.1 MB (46.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,7 +1,7 @@
 //Time Complexity: 
 //O(N), where N is the number of pixels in the image. We might process every pixel.
 //Space Complexity: O(N), the size of the implicit call stack when calling flood.
-class Solution {
+/*class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int oldcolor=image[sr][sc];
         if(oldcolor==color){
@@ -22,6 +22,28 @@ class Solution {
         }
         else{
             return;
+        }
+    }
+}*/
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[][] ans = image; 
+        dfs(sr,sc,image,ans,color,image[sr][sc]);
+        return ans;
+    }
+    public void dfs(int i,int j,int[][] image,int[][] ans,int Color,int oldcolor){
+        ans[i][j]=Color;
+        int n=image.length;
+        int m=image[0].length;
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1}; 
+        for(int k=0;k<4;k++){
+            int nrow = i + delRow[k]; 
+            int ncol = j + delCol[k]; 
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
+            image[nrow][ncol] == oldcolor && ans[nrow][ncol] != Color) {
+                dfs(nrow,ncol,image,ans,Color,oldcolor); 
+            }
         }
     }
 }
