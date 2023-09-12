8dec694dd19401798fc675710b1d0d6532fc0a68
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-09-12 22:03:50 +0530

    Time: 37 ms (42.75%), Space: 56.7 MB (13.53%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int N = nums.length;
-        for (int i = N / 2 - 1; i >= 0; i--)
-            heapify(nums, N, i);
-
-        for (int i = N - 1; i > 0; i--) {
-            int temp = nums[0];
-            nums[0] = nums[i];
-            nums[i] = temp;
-            heapify(nums, i, 0);
+        int N=nums.length;
+        for(int i=(N/2)-1;i>=0;i--){
+            heapify(nums,i,N);
+        }
+        for(int i=N-1;i>0;i--){
+            int temp=nums[i];
+            nums[i]=nums[0];
+            nums[0]=temp;
+            heapify(nums,0,i);
         }
         return nums;
     }
-    public void heapify(int arr[], int N, int i)
-    {
-        int largest = i; 
-        int l = 2 * i + 1; 
-        int r = 2 * i + 2; 
-        if (l < N && arr[l] > arr[largest])
-            largest = l;
-        
-        if (r < N && arr[r] > arr[largest])
-            largest = r;
-        
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            heapify(arr, N, largest);
+    public void heapify(int[] nums,int i,int n){
+        int largest=i;
+        int l=2*i+1;
+        int r=2*i+2;
+        if(l<n && nums[l]>nums[largest]){
+            largest=l;
+        }
+        if(r<n && nums[r]>nums[largest]){
+            largest=r;
+        }
+        if(largest!=i){
+            int temp=nums[i];
+            nums[i]=nums[largest];
+            nums[largest]=temp;
+            heapify(nums,largest,n);
         }
     }
 }
