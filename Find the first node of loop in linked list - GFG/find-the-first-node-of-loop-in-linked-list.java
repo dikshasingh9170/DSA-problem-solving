84dc23f51e997d6a56a1c28672aa789ce126e256
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-10-09 00:18:34 +0530

    Added solution - LeetHub

--- Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.java
+++ Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.java
@@ -108,11 +108,15 @@ class GFG
 class Solution {
     //Function to find first node if the linked list has a loop.
     public static int findFirstNode(Node head){
+        //code here
+        if(head==null || head.next==null){
+            return -1;
+        }
         Node slow=head;
         Node fast=head;
-        while(fast.next.next!=null && fast.next!=null){
-            fast=fast.next.next;
+        while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
+            fast=fast.next.next;
             if(slow==fast){
                 slow=head;
                 while(slow!=fast){
