48e49cd51df2fa2b294342ef99b3133062893460
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2023-12-07 17:08:18 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.java
+++ Kadane's Algorithm - GFG/kadanes-algorithm.java
@@ -32,9 +32,13 @@ class Main {
 
 
 class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
     long maxSubarraySum(int arr[], int n){
         
-        long currsum=0;long maxsum=Long.MIN_VALUE;
+        long currsum=0;long maxsum=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             currsum+=arr[i];
             if(currsum>maxsum){
