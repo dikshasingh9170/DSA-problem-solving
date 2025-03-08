debc2ec98a8e39a77aea6e23437ff409b89b826d
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-03-08 13:04:39 +0530

    Time: 3 ms (82.55%) | Memory: 42.9 MB (22.46%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        if(root==null){
+            return new ArrayList<>();
+        }
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> tm=new TreeMap<>();
+        Queue<Tuple> q=new LinkedList<Tuple>();
+        q.offer(new Tuple(root,0,0));
+        while(!q.isEmpty()){
+            int size=q.size();
+            while(size-->0){
+                TreeNode x=q.peek().root;
+                int row=q.peek().row;
+                int col=q.peek().col;
+                q.poll();
+                if(!tm.containsKey(row)){
+                    tm.put(row,new TreeMap<>());
+                }
+                if(!tm.get(row).containsKey(col)){
+                    tm.get(row).put(col,new PriorityQueue<>());
+                }
+                tm.get(row).get(col).offer(x.val);
+                if(x.left!=null){
+                    q.offer(new Tuple(x.left,row-1,col+1));
+                }
+                if(x.right!=null){
+                    q.offer(new Tuple(x.right,row+1,col+1));
+                }
+            }
+        }
+        List<List<Integer>> ans=new ArrayList<>();
+        for(TreeMap<Integer,PriorityQueue<Integer>> ys:tm.values()){
+            ArrayList<Integer> res=new ArrayList<Integer>();
+            for(PriorityQueue<Integer> pq:ys.values()){
+                while(!pq.isEmpty()){
+                    res.add(pq.poll());
+                }
+            }
+            ans.add(res);
+        }
+        return ans;
+    }
+}
+class Tuple{
+    TreeNode root;
+    int row;
+    int col;
+    public Tuple(TreeNode root,int row,int col){
+        this.root=root;
+        this.row=row;
+        this.col=col;
+    }
+}
