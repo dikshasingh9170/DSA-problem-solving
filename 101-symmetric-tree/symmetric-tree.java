91693361799c52b1540c71ef5852cfb23102e38b
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-07-13 04:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (11.84%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -21,12 +21,12 @@ class Solution {
         return solution(root.left,root.right);
     }
     public boolean solution(TreeNode root1,TreeNode root2){
-        if(root1== null && root2==null){
+        if(root1==null && root2==null){
             return true;
         }
         if(root1==null || root2==null){
             return false;
         }
-        return (root1.val==root2.val) && solution(root1.right,root2.left) && solution(root1.left,root2.right);
+        return (root1.val==root2.val) && solution(root1.left,root2.right) && solution(root1.right,root2.left);
     }
 }
