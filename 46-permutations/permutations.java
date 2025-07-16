fe414687ceb50e99f3e55604b6dab50ce492d721
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-07-16 23:20:16 +0530

    Time: 2 ms (37.03%) | Memory: 44.7 MB (44.91%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,23 +2,20 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> arr=new ArrayList<>();
-        boolean[] visited=new boolean[nums.length];
-        solution(nums,visited,arr,res);
+        solution(nums,arr,res);
         return res;
     }
-    public void solution(int[] nums,boolean[] visited,List<Integer> arr,List<List<Integer>> res){
+    public void solution(int[] nums,List<Integer> arr,List<List<Integer>> res){
         if(arr.size()==nums.length){
             res.add(new ArrayList<>(arr));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(visited[i])continue;
+            if(arr.contains(nums[i]))continue;
 
-            visited[i]=true;
             arr.add(nums[i]);
-            solution(nums,visited,arr,res);
+            solution(nums,arr,res);
             arr.remove(arr.size()-1);
-            visited[i]=false;
         }
     }
 }
