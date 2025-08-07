c5b56b0110dbc9775901a8dbd1a3d9dfea68603a
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-08-07 23:19:14 +0530

    Time: 0 ms (100%), Space: 42.1 MB (8.37%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,60 +13,18 @@
  *     }
  * }
  */
-/*class Solution {
-     public ArrayList<Integer> arr=new ArrayList<Integer>();
+class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        solve(root);
-        return arr;
+        List<Integer> res=new ArrayList<>();
+        preorder(root,res);
+        return res;
     }
-    public void solve(TreeNode root){
+    public void preorder(TreeNode root,List<Integer> res){
         if(root==null){
             return;
         }
-        arr.add(root.val);
-        solve(root.left);
-        solve(root.right);
-    }
-}*/
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();   
-        /*Stack<TreeNode> stack = new Stack<>();  
-        TreeNode node=root;                 
-        while(!stack.isEmpty() || node!=null){
-            if(node!=null){
-                stack.push(node);
-                result.add(node.val);
-                node=node.left;
-            }
-            else{
-                node=stack.pop();
-                node=node.right;
-            }
-        }
-        return result;*/
-        TreeNode curr=root;
-        while(curr!=null){
-            if(curr.left==null){
-                result.add(curr.val);
-                curr=curr.right;
-            }
-            else{
-                TreeNode prev=curr.left;
-                while(prev.right!=null && prev.right!=curr){
-                    prev=prev.right;
-                }
-                if(prev.right==null){
-                    prev.right=curr;
-                    result.add(curr.val);
-                    curr=curr.left;
-                }
-                else{
-                    prev.right=null;
-                    curr=curr.right;
-                }
-            }
-        }
-        return result;
+        res.add(root.val);
+        preorder(root.left,res);
+        preorder(root.right,res);
     }
 }
