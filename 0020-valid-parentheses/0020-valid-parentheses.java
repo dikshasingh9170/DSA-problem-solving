ae192afcef2bd150f69235027d93581e185b5e47
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-08-08 23:32:40 +0530

    Time: 3 ms (37.72%), Space: 41.7 MB (65.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,36 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<Character>();
-        if(s.length()==0){
-            return true;
-        }
-        st.push(s.charAt(0));char x=0;int flag=0;
-        for(int i=1;i<s.length();i++){
-            if(!st.isEmpty()){
-                x=st.pop();
-                if(s.charAt(i)==')' && x=='('){
-                    continue;
-                }
-                else if(s.charAt(i)=='}' && x=='{'){
-                    continue;
-                }
-                else if(s.charAt(i)==']' && x=='['){
-                    continue;
-                }
-                else if(s.charAt(i)==')' || s.charAt(i)==']' || s.charAt(i)=='}'){
-                    return false;
-                }
-                st.push(x);
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
                 st.push(s.charAt(i));
-                x=0;
-            }
-            else{
-                st.push(s.charAt(i));
+            }else{
+                if(!st.isEmpty()){
+                    char x=st.peek();
+                    if(x=='(' && s.charAt(i)==')' || x=='{' && s.charAt(i)=='}' || x=='[' && s.charAt(i)==']'){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s.charAt(i));
+                    }
+                }
+                else{
+                    st.push(s.charAt(i));
+                }
+                
             }
         }
-            if(st.size()==0){
-            return true;
-            }
-        return false;
+        return st.isEmpty()?true:false;
     }
 }
