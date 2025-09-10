d51901edd256a599fa31359263f44fe30b6d3989
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-09-10 14:33:19 +0530

    Time: 6 ms (72.34%), Space: 43.7 MB (91.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,27 +1,43 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
-        int count=0;int max=0;
+        /*HashSet<Character> hs=new HashSet<Character>();
+        int l=0;int r=1;int count=1;int x=1;
         if(s.length()==0){
             return 0;
-        }int x=0;
-        for(int i=0;i<s.length();i++){
-            if(hm.containsKey(s.charAt(i))){
-                if(hm.get(s.charAt(i))>=x){
-                    count=i-hm.get(s.charAt(i));
-                }
-                else{
-                    count++;
-                }
-                x=Math.max(x,hm.get(s.charAt(i)));
-                hm.put(s.charAt(i),i);
-            }
-            else{
-                hm.put(s.charAt(i),i);
-                count++;
-            }
-            max=Math.max(count,max);
         }
-        return max;
+        if(s.equals(" ") || s.length()==1){
+            return 1;
+        }
+        while(l<s.length()){
+	        hs.add(s.charAt(l));
+	        while(r<s.length()){
+	            hs.add(s.charAt(r));
+                x++;
+	            if(x!=hs.size()){
+                    break;
+                }
+	            count=Math.max(count,hs.size());
+	            r++;
+	        }
+            x=1;
+	        hs.clear();
+	        l++;
+	        r=l+1;
+	    }
+        return count;*/
+        HashMap<Character, Integer> st = new HashMap<>();
+        int ans = 0, l = 0, r = 0;
+        
+        while (r < s.length()) {
+            if (st.containsKey(s.charAt(r))) {
+                l = Math.max(l, st.get(s.charAt(r)) + 1);
+            }
+            st.put(s.charAt(r), r);
+            
+            ans = Math.max(ans, r - l + 1);
+            r++;
+        }
+        
+        return ans;
     }
 }
