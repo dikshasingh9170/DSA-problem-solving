88da86cc80b983811a98e8ae9a741a3d1f67add2
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-09-10 20:53:02 +0530

    Time: 45 ms (66.4%), Space: 114.2 MB (55.22%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,3 +1,64 @@
+class Node{
+    int key,value;
+    Node next,prev;
+    public Node(int key,int value){
+        this.key=key;
+        this.value=value;
+    }
+}
+class LRUCache {
+    public int capacity;
+    Map<Integer,Node> map;
+    Node head,tail;
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        this.map=new HashMap<>();
+        head=new Node(0,0);
+        tail=new Node(0,0);
+        head.next=tail;
+        tail.prev=head;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)){
+            return -1;
+        }
+        Node node=map.get(key);
+        remove(node);
+        insertAtFront(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            Node node=map.get(key);
+            node.value=value;
+            remove(node);
+            insertAtFront(node);
+            map.put(key,node);
+        }
+        else{
+            if(map.size()==capacity){
+                Node lru=tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node node=new Node(key,value);
+            map.put(key,node);
+            insertAtFront(node);
+        }
+    }
+    public void remove(Node node){
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    }
+    public void insertAtFront(Node node){
+       node.next=head.next;
+       node.prev=head;
+       head.next.prev=node;
+       head.next=node;
+    }
+}
 
 /**
  * Your LRUCache object will be instantiated and called as such:
@@ -5,19 +66,3 @@
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
-class LRUCache { 
-    private final LinkedHashMap<Integer, Integer> cache;
-    public LRUCache(int capacity) {
-        this.cache = new LinkedHashMap<Integer, Integer>(capacity, .75f, true){
-            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
-                return size() > capacity;
-            }
-        };
-    }
-    public int get(int key) {
-       return cache.getOrDefault(key, -1);
-    }
-    public void put(int key, int value) {
-        cache.put(key, value);
-    }
-}
