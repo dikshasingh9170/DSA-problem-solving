b9be5bb36e66ec47d34741a6dff403ca0902db31
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-12-20 15:13:07 +0530

    Time: 1 ms (78.09%) | Memory: 42.2 MB (60.74%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do{
+            slow=square(slow);
+            fast=square(square(fast));
+        }while(slow!=fast);
+
+        return slow==1;
+    }
+    public int square(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem*rem;
+            n=n/10;
+        }
+        return sum;
+    }
+}
