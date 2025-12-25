42309bb0db75172c7f3721c80a1873ee3dfc90fd
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-12-26 00:05:47 +0530

    Time: 6 ms (84.37%) | Memory: 45.5 MB (20.79%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,14 +1,15 @@
 class Solution {
     public String reverseWords(String s) {
         String[] str=s.trim().split(" ");
-        String out = "";
+        StringBuilder out =new StringBuilder();
 
         for (int i = str.length - 1; i >=0; i--) {
             if(str[i]!=""){
-                out += str[i] + " ";
+                out.append(str[i]+" ");
+
             }
         }
 
-        return out.trim();
+        return out.toString().trim();
     }
 }
