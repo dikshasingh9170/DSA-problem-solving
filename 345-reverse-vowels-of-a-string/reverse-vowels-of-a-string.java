b6fad17e4b655f5cc47e7cd14bfd9970008ad4b9
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2025-12-26 00:38:36 +0530

    Time: 2 ms (99.15%) | Memory: 46.8 MB (33.50%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -2,20 +2,22 @@ class Solution {
     public String reverseVowels(String s) {
         StringBuilder vowels=new StringBuilder();
         char[] s1=s.toCharArray();
-        for(int i=0;i<s.length();i++){
-            if(s1[i]=='a' || s1[i]=='e' || s1[i]=='i' || s1[i]=='o' || s1[i]=='u' || s1[i]=='A' || s1[i]=='E' || s1[i]=='I' || s1[i]=='O' || s1[i]=='U'){
-                vowels.append(s.charAt(i));
-            }
-        }
-        int n=vowels.length();
-        int j=n-1;
-        String v=vowels.toString();
-        for(int i=0;i<s1.length;i++){
-            if(s1[i]=='a' || s1[i]=='e' || s1[i]=='i' || s1[i]=='o' || s1[i]=='u' || s1[i]=='A' || s1[i]=='E' || s1[i]=='I' || s1[i]=='O' || s1[i]=='U'){
-                s1[i]=v.charAt(j);
-                j--;
-            }
+        int l=0;int r=s1.length-1;
+        while(l<r){
+            while(l<r && !isVowel(s1[l]))l++;
+            while(l<r && !isVowel(s1[r]))r--;
+            char temp=s1[l];
+            s1[l]=s1[r];
+            s1[r]=temp;
+            l++;
+            r--;
         }
         return new String(s1);
+    }
+    public boolean isVowel(char s1){
+        if(s1=='a' || s1=='e' || s1=='i' || s1=='o' || s1=='u' || s1=='A' || s1=='E' || s1=='I' || s1=='O' || s1=='U'){
+                return true;
+        }
+        return false;
     }
 }
