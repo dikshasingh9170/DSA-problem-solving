5f5fd7c954f0e366e80ba6c4ccd3ea9476ca0fb5
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2026-01-14 01:50:41 +0530

    Time: 3 ms (87.28%) | Memory: 52 MB (73.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,48 +2,26 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
+        int cnt=0;
         int[][] vis=new int[n][m];
-        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 && grid[i][j]=='1'){
-                    count++;
-                    dfs(i,j,grid,vis);
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    dfs(i,j,grid);
                 }
             }
         }
-        return count;
+        return cnt;
     }
-    public void dfs(int row,int col,char[][] grid,int[][] vis){
-        Queue<int[]> q=new LinkedList<>();
-        q.offer(new int[]{row,col});
-        vis[row][col]=1;
-        while(!q.isEmpty()){
-            int[] res=q.poll();
-            row=res[0];
-            col=res[1];
-            if(isValid(row-1,col,grid,vis)){
-                q.offer(new int[]{row-1,col});
-                vis[row-1][col]=1;
-            }
-            if(isValid(row+1,col,grid,vis)){
-                q.offer(new int[]{row+1,col});
-                vis[row+1][col]=1;
-            }
-            if(isValid(row,col-1,grid,vis)){
-                q.offer(new int[]{row,col-1});
-                vis[row][col-1]=1;
-            }
-            if(isValid(row,col+1,grid,vis)){
-                q.offer(new int[]{row,col+1});
-                vis[row][col+1]=1;
-            }
+    public void dfs(int row,int col,char[][] grid){
+        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]=='0'){
+            return;
         }
-    }
-    public boolean isValid(int row,int col,char[][] grid,int[][] vis){
-        if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && vis[row][col]!=1 && grid[row][col]=='1'){
-            return true;
-        }
-        return false;
+        grid[row][col]='0';
+        dfs(row-1,col,grid);
+        dfs(row+1,col,grid);
+        dfs(row,col+1,grid);
+        dfs(row,col-1,grid);
     }
 }
