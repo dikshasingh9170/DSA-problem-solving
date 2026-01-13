9fc47ee670a537fcb4c58d634a109374c418cce0
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2026-01-13 20:22:40 +0530

    Time: 5 ms (44.23%) | Memory: 47 MB (43.03%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;
+
+    public int[] findMode(TreeNode root) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        helper(root, map);
+
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (e.getValue() == max) {
+                list.add(e.getKey());
+            }
+        }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public void helper(TreeNode root, HashMap<Integer, Integer> map) {
+        if (root == null) return;
+
+        int freq = map.getOrDefault(root.val, 0) + 1;
+        map.put(root.val, freq);
+        max = Math.max(max, freq);
+
+        helper(root.left, map);
+        helper(root.right, map);
+    }
+}
