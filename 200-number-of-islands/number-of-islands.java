404d32e64a8fdd218025416bdaef09b08338557d
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2026-01-14 01:05:47 +0530

    Time: 6 ms (24.39%) | Memory: 51.5 MB (82.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,45 +3,45 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int[][] vis=new int[n][m];
-        int cnt=0;
+        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]=='1'){
-                    cnt++;
-                    bfs(i,j,grid,vis);
+                    count++;
+                    dfs(i,j,grid,vis);
                 }
             }
         }
-        return cnt;
+        return count;
     }
-    public void bfs(int row,int col,char[][] grid,int[][] vis){
-        Queue<int[]> q=new ArrayDeque<>();
-        q.add(new int[]{row,col});
+    public void dfs(int row,int col,char[][] grid,int[][] vis){
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{row,col});
         vis[row][col]=1;
         while(!q.isEmpty()){
             int[] res=q.poll();
             row=res[0];
             col=res[1];
-            if(isValid(row+1,col,grid,vis)){
-                vis[row+1][col]=1;
-                q.add(new int[]{row+1,col});
-            }
             if(isValid(row-1,col,grid,vis)){
+                q.offer(new int[]{row-1,col});
                 vis[row-1][col]=1;
-                q.add(new int[]{row-1,col});
             }
-            if(isValid(row,col+1,grid,vis)){
-                vis[row][col+1]=1;
-                q.add(new int[]{row,col+1});
+            if(isValid(row+1,col,grid,vis)){
+                q.offer(new int[]{row+1,col});
+                vis[row+1][col]=1;
             }
             if(isValid(row,col-1,grid,vis)){
+                q.offer(new int[]{row,col-1});
                 vis[row][col-1]=1;
-                q.add(new int[]{row,col-1});
+            }
+            if(isValid(row,col+1,grid,vis)){
+                q.offer(new int[]{row,col+1});
+                vis[row][col+1]=1;
             }
         }
     }
     public boolean isValid(int row,int col,char[][] grid,int[][] vis){
-        if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && vis[row][col]==0 && grid[row][col]=='1'){
+        if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && vis[row][col]!=1 && grid[row][col]=='1'){
             return true;
         }
         return false;
