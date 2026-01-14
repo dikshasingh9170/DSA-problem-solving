58dcdcd36b30a215690a974b2cc9c921a61989df
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2026-01-14 15:27:16 +0530

    Time: 2 ms (100.00%) | Memory: 46.4 MB (63.06%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.java
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -1,28 +1,27 @@
 class Solution {
     public int closedIsland(int[][] grid) {
-        int rows = grid.length, cols = grid[0].length, count = 0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 0 && dfs(grid,i, j)) {
-                    count++;
+        int cnt=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==0 && dfs(i,j,grid)){
+                    cnt++;
                 }
             }
         }
-        
-        return count;
+        return cnt;
     }
-    public boolean dfs(int[][] grid,int i, int j) {
-        int rows = grid.length, cols = grid[0].length;
-        if (i < 0 || j < 0 || i >= rows || j >= cols) {
+    public boolean dfs(int i,int j,int[][] grid){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length){
             return false;
         }
-        if (grid[i][j] == 1) {
+        if(grid[i][j]==1){
             return true;
         }
-        grid[i][j] = 1; // mark as visited
-        boolean left = dfs(grid,i, j-1), right = dfs(grid,i, j+1);
-        boolean up = dfs(grid,i-1, j), down = dfs(grid,i+1, j);
+        grid[i][j]=1;
+        boolean left=dfs(i,j-1,grid);
+        boolean right=dfs(i,j+1,grid);
+        boolean up=dfs(i-1,j,grid);
+        boolean down=dfs(i+1,j,grid);
         return left && right && up && down;
     }
 }
