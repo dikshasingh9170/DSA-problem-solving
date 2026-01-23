4f2152d6658cebca29a949a71679826544f8cd41
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2026-01-23 22:03:24 +0530

    Time: 1 ms (86.91%) | Memory: 47.3 MB (67.83%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,24 +1,70 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean[] vis = new boolean[n];
-        int provinces = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                provinces++;
-                dfs(i, isConnected, vis);
+        int n=isConnected.length;
+        DisjointSet ds = new DisjointSet(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    ds.unionByRank(i,j);
+                }
             }
         }
-        return provinces;
+        for(int i=0;i<n;i++){
+            if(ds.findUPar(i)==i){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+class DisjointSet {
+    private int[] rank;
+    private int[] size;
+    private int[] parent;
+    public DisjointSet(int n) {
+        rank = new int[n + 1];
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        Arrays.fill(size, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+    public int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } 
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
     }
 
-    private void dfs(int start, int[][] isConnected, boolean[] vis) {
-        vis[start]=true;
-        for(int j=0;j<isConnected[start].length;j++){
-            if(isConnected[start][j]==1 && !vis[j]){
-                dfs(j,isConnected,vis);
-            }
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 }
+
+
