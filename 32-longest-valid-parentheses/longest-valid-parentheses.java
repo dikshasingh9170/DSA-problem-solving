983f10acb7792287386bcb2433c7b28031ba4fea
DIKSHA SINGH <99008393+dikshasingh9170@users.noreply.github.com>
2026-01-25 19:16:40 +0530

    Time: 5 ms (77.92%) | Memory: 46.6 MB (49.15%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,42 +1,22 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Pair> st=new Stack<>();
-        int max=0;
-        for(int i=0;i<s.length();i++){
-            if(!st.isEmpty()){
-                Pair curr=st.peek();
-                char currCh=curr.ch;
-                int idx=curr.idx;
-                if(s.charAt(i)==')' && currCh=='('){
-                    st.pop();
-                }
-                else{
-                    st.push(new Pair(s.charAt(i),i));
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int max_len = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    max_len = Math.max(max_len, i - stack.peek());
                 }
             }
-            else{
-                st.push(new Pair(s.charAt(i),i));
-            }
         }
-        int len=s.length();
-        if(st.isEmpty()){
-            return len;
-        }
-        while(!st.isEmpty()){
-            Pair curr=st.pop();
-            int idx=curr.idx;
-            max=Math.max(max,len-idx-1);
-            len=idx;
-        }
-        max=Math.max(max,len);
-        return max;
-    }
-}
-class Pair{
-    char ch;
-    int idx;
-    public Pair(char ch,int idx){
-        this.ch=ch;
-        this.idx=idx;
+
+        return max_len;        
     }
 }
